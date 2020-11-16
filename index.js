e843dc69eafdcf0755a28b5e7fc476714cfdc290
JulienQHN <julien.quennehen@edu.devinci.fr>
2020-11-16 14:47:10 +0000

    I forgot to Commit last week  fewd_lab_7

---
+++ index.js
@@ -0,0 +1,45 @@
+var firstName = 'Julien';
+var favouriteColor = 'Vert';
+var city = 'Lubumbashi'
+var country = 'Congo';
+var city = 'Houilles'; 
+var favouriteFood = "Miel";
+
+console.log(firstName)
+console.log(favouriteColor)
+console.log(favouriteFood)
+console.log(city)
+// Le code affiche dans la console les valeurs des variables écrites dans le consol.log.    
+
+var phrase = "A rainbow is formed by the sunlight. Shining through rain drops sosmall. The rain bends the light from the sunshine, and colors the sky for us all."
+var endingPhrase = "is what we see. Rainbow colours, are made with light bending you see!"
+var one = "Red"
+var two = "orange" 
+var three = "yellow"
+var four = "green"
+var five = "blue"
+var six = "indigo"
+var seven = "violet"    
+
+console.log(phrase, one, two, three, four, five, six, seven, endingPhrase)
+
+var myAlphabet = ['A', 'B', 'C']
+
+function myAlphabetLength() {
+   console.log(myAlphabet.length)
+}
+
+
+var x = 5, y = 10, z = 15;
+
+x+y;
+y - x;
+x * y;
+y/x;
+x%2;
+x++;
+x--;
+
+console.log(x--)
+
+
